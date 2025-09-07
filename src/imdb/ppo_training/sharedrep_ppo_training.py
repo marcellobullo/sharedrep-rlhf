02c7c57e8bdbe4fe44886789fc4beec32a84796d
mb <mb1921@ic.ac.uk>
2025-09-07 22:25:49 +0000

    fix imdb ppo sharedrep

--- src/imdb/ppo_training/sharedrep_ppo_training.py
+++ src/imdb/ppo_training/sharedrep_ppo_training.py
@@ -6,20 +6,21 @@ sys.path.insert(0, ROOT)
 
 import argparse
 import importlib
-from trl import PPOConfig, PPOTrainer
 from datasets import load_dataset
 from accelerate import Accelerator
+from trl import PPOConfig, PPOTrainer
+from trl.trainer.utils import get_reward
 from src.helper.imdb_utils import SharedRepPPOTrainer
 from src.models.sr_gpt2.sr_gpt2_modeling import SharedRepGPT2RM
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSequenceClassification
-import argparse
+from src.helper.imdb_utils import pre_processing_imdb
+
 
 
 import torch
 from trl.trainer.utils import first_true_indices
-def get_sharedrep_reward(
-    model: torch.nn.Module, query_responses: torch.Tensor, pad_token_id: int, context_length: int
-) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+
+def get_sharedrep_reward( model: torch.nn.Module, query_responses: torch.Tensor, pad_token_id: int, context_length: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
     Computes the reward logits and the rewards for a given model and query responses.
 
@@ -42,38 +43,39 @@ def get_sharedrep_reward(
             - `sequence_lengths` (`torch.Tensor`):
                 The lengths of the sequences in the query responses.
     """
-    assert model.maxmin, "The model cannot be in maxmin mode"
-    pos_id = model.config.label2id["POSITIVE"]
+    if not isinstance(model, SharedRepGPT2RM):
+        return get_reward(model, query_responses, pad_token_id, context_length)
     
-    attention_mask = query_responses != pad_token_id
-    position_ids = attention_mask.cumsum(1) - attention_mask.long()  # exclusive cumsum
-    input_ids = torch.masked_fill(query_responses, ~attention_mask, 0)
+    elif isinstance(model, SharedRepGPT2RM):
+        assert model.maxmin, "The model must be in maxmin mode"
 
-    # Sentiment score
-    sentiment_scores = model(
-        input_ids=input_ids,
-        attention_mask=attention_mask,
-        position_ids=position_ids,
-        return_dict=True,
-        output_hidden_states=True,
-        use_cache=False,  # otherwise mistral-based RM would error out
-    )
-    probs = torch.softmax(sentiment_scores.logits, dim=-1)
-    positiveness = probs[:, pos_id]
+        attention_mask = query_responses != pad_token_id
+        position_ids = attention_mask.cumsum(1) - attention_mask.long()  # exclusive cumsum
+        input_ids = torch.masked_fill(query_responses, ~attention_mask, 0)
 
-    # Conciseness
-    sequence_lengths = first_true_indices(query_responses[:, context_length:] == pad_token_id) - 1 + context_length
-    conciseness = 1-(sequence_lengths/100)
+        # Compute rewards
+        rewards = model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+        )
     
-    # Compute both group scores
-    group_score_majority = conciseness
-    group_score_minority = 0.3 * conciseness+ 0.7 * positiveness
+        sequence_lengths = first_true_indices(query_responses[:, context_length:] == pad_token_id) - 1 + context_length
+        
+        return (
+            rewards.logits,
+            rewards.logits.squeeze(-1),
+            sequence_lengths,
+        )
 
-    rewards = torch.min(group_score_majority, group_score_minority)
-    return (
-        rewards,
-        rewards.squeeze(-1),
-        sequence_lengths,
+def tokenize(examples, tokenizer):
+    return tokenizer(
+        examples["query"],
+        padding="max_length",
+        max_length=8,
+        truncation=True,
+        padding_side="left",
+        return_tensors="pt"
     )
 
 
@@ -100,12 +102,8 @@ if __name__ == "__main__":
 
     # Dataset
     batch_size = 16
-    dataset_name = f"{user_id}/gpt2-imdb-raw"
+    dataset_name = f"stanfordnlp/imdb"
     dataset = load_dataset(dataset_name, split="train")
-    with accelerator.main_process_first():
-        dataset = dataset.train_test_split(test_size=0.05, seed=seed)
-        train_dataset = dataset["train"]
-        eval_dataset = dataset["test"]
 
 
     # Reward Model
@@ -114,7 +112,7 @@ if __name__ == "__main__":
     reward_model.enable_maxmin()
     
     # Reward Tokenizer
-    reward_tokenizer = AutoTokenizer.from_pretrained(reward_model_name)
+    reward_tokenizer = AutoTokenizer.from_pretrained(reward_model_name, padding_side="left")
     reward_tokenizer.pad_token = reward_tokenizer.eos_token
     reward_model.config.pad_token_id = reward_tokenizer.pad_token_id
     reward_model.eval()
@@ -134,12 +132,31 @@ if __name__ == "__main__":
     # Value model
     value_model = AutoModelForSequenceClassification.from_pretrained( policy_model_name, trust_remote_code=True, num_labels=1)
 
+    with accelerator.main_process_first():
+
+        min_review_length = 200
+        input_min_text_length = 2
+        input_max_text_length = 8
+        
+        dataset = pre_processing_imdb(
+            dataset,
+            model_name=policy_model_name,
+            min_review_length=min_review_length,
+            input_min_text_length=input_min_text_length,
+            input_max_text_length=input_max_text_length
+        )
+        dataset = dataset.map(lambda x: tokenize(x, policy_tokenizer), batched=True, batch_size=batch_size, num_proc=32, desc="Tokenizing")
+        dataset.set_format(type="torch", columns=["input_ids", "attention_mask"])
+        dataset = dataset.train_test_split(test_size=0.05, seed=seed)
+        train_dataset = dataset["train"]
+        eval_dataset = dataset["test"]
+
     # Training Arguments
     training_args = PPOConfig(
         logging_steps=10,
         num_train_epochs=5,
         num_ppo_epochs=3,
-        per_device_train_batch_size=128,
+        per_device_train_batch_size=64,
         gradient_accumulation_steps=1,
         bf16=True,
         learning_rate= 3e-06,
@@ -155,8 +172,7 @@ if __name__ == "__main__":
         total_episodes = 50000,
         lr_scheduler_type= "cosine",
         stop_token= "eos",
-        kl_coef=0.05
-
+        kl_coef=0.05,
     )
 
     m = importlib.import_module(PPOTrainer.__module__)  
@@ -173,19 +189,6 @@ if __name__ == "__main__":
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
     )
-    # trainer = SharedRepPPOTrainer(
-    #     pluralistic_reward_model=reward_model,#.to(device),
-    #     args=training_args,
-    #     processing_class=policy_tokenizer,
-    #     model=policy,
-    #     ref_model=ref_policy,
-    #     reward_model=reward_model,  # place-holder
-    #     value_model=value_model,
-    #     train_dataset=train_dataset,
-    #     eval_dataset=eval_dataset,
-    #     use_tanh=True,
-    #     pointwise=True
-    # )
     trainer.train()
     
     # Save the model
